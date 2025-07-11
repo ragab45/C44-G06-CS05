d4235c92de1355d33a78827c1906b6a1f38e018b
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-07-11 20:32:33 +0300

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Video_1
+
+            #endregion;
+        }
+    }
+}
