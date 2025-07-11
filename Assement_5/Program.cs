a680aa394853a213c46367031a09fbd0fd9f4f13
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-07-11 20:57:17 +0300

    Q-7

--- Assement_5/Program.cs
+++ Assement_5/Program.cs
@@ -27,73 +27,82 @@
             Console.WriteLine($"Sum of array elements: {sum}");
             #endregion
             #region Q-3
-            int[] arr1 = { 2, 4, 6 };
-            int[] arr2 = { 1, 3, 5 };
-            int[] merged = new int[arr1.Length + arr2.Length];
+            //int[] arr1 = { 2, 4, 6 };
+            //int[] arr2 = { 1, 3, 5 };
+            //int[] merged = new int[arr1.Length + arr2.Length];
 
-            arr1.CopyTo(merged, 0);
-            arr2.CopyTo(merged, arr1.Length);
+            //arr1.CopyTo(merged, 0);
+            //arr2.CopyTo(merged, arr1.Length);
 
-            Array.Sort(merged);
-            Console.WriteLine("Merged and sorted array: " + string.Join(", ", merged));
-            #endregion
-            #region Q-4
-            int[] arr = { 2, 4, 2, 6, 6, 8, 9, 10, 30, 59, 58 };
-            int max = arr[0], min = arr[0];
+            //Array.Sort(merged);
+            //Console.WriteLine("Merged and sorted array: " + string.Join(", ", merged));
+            //#endregion
+            //#region Q-4
+            //int[] arr = { 2, 4, 2, 6, 6, 8, 9, 10, 30, 59, 58 };
+            //int max = arr[0], min = arr[0];
 
-            foreach (int val in arr)
-            {
-                if (val > max) max = val;
-                if (val < min) min = val;
-            }
+            //foreach (int val in arr)
+            //{
+            //    if (val > max) max = val;
+            //    if (val < min) min = val;
+            //}
 
-            Console.WriteLine($"Max: {max}, Min: {min}");
+            //Console.WriteLine($"Max: {max}, Min: {min}");
             #endregion
             #region Q-5
-            int[] arr = { 10, 20, 30, 40, 50, 50 };
-            int largest = int.MinValue, second = int.MinValue;
+            //int[] arr = { 10, 20, 30, 40, 50, 50 };
+            //int largest = int.MinValue, second = int.MinValue;
 
-            foreach (int num in arr)
-            {
-                if (num > largest)
-                {
-                    second = largest;
-                    largest = num;
-                }
-                else if (num > second && num != largest)
-                {
-                    second = num;
-                }
-            }
+            //foreach (int num in arr)
+            //{
+            //    if (num > largest)
+            //    {
+            //        second = largest;
+            //        largest = num;
+            //    }
+            //    else if (num > second && num != largest)
+            //    {
+            //        second = num;
+            //    }
+            //}
 
-            Console.WriteLine($"Second largest: {second}");
+            //Console.WriteLine($"Second largest: {second}");
             #endregion
             #region Q-6
-            Console.Write("Enter array elements (space separated): ");
-            string[] input = Console.ReadLine().Split();
-            int[] arr = Array.ConvertAll(input, int.Parse);
+            //    Console.Write("Enter array elements (space separated): ");
+            //    string[] input = Console.ReadLine().Split();
+            //    int[] arr = Array.ConvertAll(input, int.Parse);
 
-            Dictionary<int, int> firstIndex = new();
-            int maxDistance = 0;
+            //    Dictionary<int, int> firstIndex = new();
+            //    int maxDistance = 0;
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (firstIndex.ContainsKey(arr[i]))
-                {
-                    int distance = i - firstIndex[arr[i]] - 1;
-                    if (distance > maxDistance)
-                        maxDistance = distance;
-                }
-                else
-                {
-                    firstIndex[arr[i]] = i;
-                }
-            }
+            //    for (int i = 0; i < arr.Length; i++)
+            //    {
+            //        if (firstIndex.ContainsKey(arr[i]))
+            //        {
+            //            int distance = i - firstIndex[arr[i]] - 1;
+            //            if (distance > maxDistance)
+            //                maxDistance = distance;
+            //        }
+            //        else
+            //        {
+            //            firstIndex[arr[i]] = i;
+            //        }
+            //    }
 
-            Console.WriteLine($"Longest distance between two equal cells: {maxDistance}");
+            //    Console.WriteLine($"Longest distance between two equal cells: {maxDistance}");
+            //}
+            #endregion
+            #region Q-7
+            Console.Write("Enter words: ");
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(' ');
+            Array.Reverse(words);
+
+            Console.WriteLine(string.Join(" ", words));
+
+
         }
-        #endregion  
-
-    }
 }
 }
