a38432b2b8b305f2ac39fc220d7c253745eaa577
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-07-11 20:50:47 +0300

    Q-1

--- Assement_5/Program.cs
+++ Assement_5/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            #region Q-1
+            Console.Write("Enter size of identity matrix: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(i == j ? "1 " : "0 ");
+                }
+                Console.WriteLine();
+                #endregion
+            }
     }
 }
